9935e4bd3e3dd676b0f1f7f7701ac3530f947453
zvz09 <18237119598@163.com>
2023-11-23 20:39:25 +0800

    文件管理模块

--- xiaoxiao-ui/xiaochen-sysytem-ui/src/stores/modules/dictionary.ts
+++ xiaoxiao-ui/xiaochen-sysytem-ui/src/stores/modules/dictionary.ts
@@ -1,41 +1,35 @@
 import { getAllByDictionaryEncode } from "@/api/system/dictionary";
 import { defineStore } from "pinia";
-import { reactive, toRaw, Ref } from "vue";
+import { reactive, Ref, toRaw } from "vue";
 import { Dictionary } from "@/api/system/dictionary/types";
 import { ResultData } from "@/api/interface";
 
-// 定义字典映射的类型
-type DictionaryMap = Record<string, Dictionary.DictionaryDetailVO[]>;
-type DictionaryResultDataMap = Record<string, ResultData<Dictionary.DictionaryDetailVO[]>>;
-
 export const useDictionaryStore = defineStore("dictionary", () => {
-  // 使用 Ref 泛型来明确 dictionaryMap 的类型
-  const dictionaryMap: Ref<DictionaryMap> = reactive({});
-  // 使用 Ref 泛型来明确 dictionaryResultDataMap 的类型
-  const dictionaryResultDataMap: Ref<DictionaryResultDataMap> = reactive({});
+  const dictionaryMap: Ref<Record<string, Dictionary.DictionaryDetailVO[]>> = reactive({});
+  const dictionaryResultDataMap: Ref<Record<string, ResultData<Dictionary.DictionaryDetailVO[]>>> = reactive({});
 
   const setDictionaryMaps = (encode: string, dictionaryRes: ResultData<Dictionary.DictionaryDetailVO[]>) => {
-    dictionaryResultDataMap.value[encode] = toRaw(dictionaryRes);
-    dictionaryMap.value[encode] = toRaw(dictionaryRes.data);
+    dictionaryResultDataMap[encode] = toRaw(dictionaryRes);
+    dictionaryMap[encode] = toRaw(dictionaryRes.data);
   };
 
   const getDictionary = async (encode: string): Promise<Dictionary.DictionaryDetailVO[] | null> => {
-    if (dictionaryMap.value[encode]?.length) {
-      return dictionaryMap.value[encode];
+    if (dictionaryMap[encode]?.length) {
+      return dictionaryMap[encode];
     } else {
       const data = await getAllByDictionaryEncode(encode);
       setDictionaryMaps(encode, data);
-      return dictionaryMap.value[encode];
+      return dictionaryMap[encode];
     }
   };
 
   const getDictionaryResultData = async (encode: string): Promise<ResultData<Dictionary.DictionaryDetailVO[]> | null> => {
-    if (dictionaryResultDataMap.value[encode]?.data) {
-      return dictionaryResultDataMap.value[encode];
+    if (dictionaryResultDataMap[encode]?.data) {
+      return dictionaryResultDataMap[encode];
     } else {
       const data = await getAllByDictionaryEncode(encode);
       setDictionaryMaps(encode, data);
-      return dictionaryResultDataMap.value[encode];
+      return dictionaryResultDataMap[encode];
     }
   };
 
