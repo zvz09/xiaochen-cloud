1a927d67d10da16b2e90f5ea80b3872f6d67428f
zvz09 <18237119598@163.com>
2023-12-14 11:25:00 +0800

    动态加载类

---
+++ xiaochen-modules/xiaochen-blog/src/main/java/com/zvz09/xiaochen/blog/mapper/ReptileClassMapper.java
@@ -0,0 +1,16 @@
+package com.zvz09.xiaochen.blog.mapper;
+
+import com.zvz09.xiaochen.blog.domain.entity.ReptileClass;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 爬取数据类 Mapper 接口
+ * </p>
+ *
+ * @author zvz09
+ * @since 2023-12-13
+ */
+public interface ReptileClassMapper extends BaseMapper<ReptileClass> {
+
+}
---
+++ xiaochen-modules/xiaochen-blog/src/main/java/com/zvz09/xiaochen/blog/mapper/ReptileDocumentMapper.java
@@ -0,0 +1,16 @@
+package com.zvz09.xiaochen.blog.mapper;
+
+import com.zvz09.xiaochen.blog.domain.entity.ReptileDocument;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 爬取的原始数据 Mapper 接口
+ * </p>
+ *
+ * @author zvz09
+ * @since 2023-12-13
+ */
+public interface ReptileDocumentMapper extends BaseMapper<ReptileDocument> {
+
+}
